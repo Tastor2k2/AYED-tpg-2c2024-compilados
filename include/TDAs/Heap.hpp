46b4d7450b7642ec2639de552ca6832c2a668266
gonzalo <pgarcial@fi.uba.ar>
2024-11-21 16:46:50 -0300

    Implementacion de Heap

--- include/TDAs/Heap.hpp
+++ include/TDAs/Heap.hpp
@@ -19,6 +19,11 @@ template<typename T>
 class Heap {
 private:
     Vector<T> datos;
+    bool es_maxima;
+
+    void ordenar_hacia_arriba(size_t indice);
+
+    void ordenar_hacia_abajo(size_t indice);
 
 public:
     // Constructor default.
@@ -54,34 +59,90 @@ public:
 
 template<typename T>
 Heap<T>::Heap() {
+    es_maxima = true;
 }
 
 template<typename T>
 Heap<T>::Heap(bool maxima) {
+    es_maxima = maxima;
 }
 
 template<typename T>
 void Heap<T>::alta(T dato) {
+    datos.alta(dato);
+    ordenar_hacia_arriba(datos.tamanio() - 1);
 }
 
 template<typename T>
 T Heap<T>::baja() {
+    if (vacio()) {
+        throw ExcepcionHeap("El heap esta vacio.");
+    }
+    T dato_eliminado = datos[0];
+
+    datos[0] = datos[datos.tamanio() - 1];
+    datos.baja();
+
+    ordenar_hacia_abajo(0);
+    return dato_eliminado;
 }
 
 template<typename T>
 T Heap<T>::primero() {
+    if (vacio()) {
+        throw ExcepcionHeap("El heap esta vacio.");
+    }
+    return datos[0];
 }
 
 template<typename T>
 bool Heap<T>::vacio() {
+    return datos.vacio();
 }
 
 template<typename T>
 size_t Heap<T>::tamanio() {
+    return datos.tamanio();
 }
 
 template<typename T>
 Heap<T>::~Heap() {
 }
 
+template<typename T>
+void Heap<T>::ordenar_hacia_arriba(size_t indice) {
+    size_t padre = (indice - 1) / 2;
+    while (indice > 0 && (es_maxima ?  datos[indice] > datos[padre] : datos[indice] < datos[padre])) {
+        T temp = datos[indice];
+        datos[indice] = datos[padre];
+        datos[padre] = temp;
+
+        indice = padre;
+        padre = (indice - 1) / 2;
+    }
+}
+
+template<typename T>
+void Heap<T>::ordenar_hacia_abajo(size_t indice) {
+    size_t hijo_izq = 2 * indice + 1;
+    size_t hijo_der = 2 * indice + 2;
+    size_t hijo_prometedor = indice;
+
+    if (hijo_izq < datos.tamanio() && (es_maxima ? datos[hijo_izq] > datos[hijo_prometedor] : datos[hijo_izq] < datos[hijo_prometedor])) {
+        hijo_prometedor = hijo_izq;
+    }
+
+    if (hijo_der < datos.tamanio() && (es_maxima ? datos[hijo_der] > datos[hijo_prometedor] : datos[hijo_der] < datos[hijo_prometedor])) {
+        hijo_prometedor = hijo_der;
+    }
+
+    if (hijo_prometedor != indice) {
+        T temp = datos[indice];
+        datos[indice] = datos[hijo_prometedor];
+        datos[hijo_prometedor] = temp;
+        ordenar_hacia_abajo(hijo_prometedor);
+    }
+}
+
 #endif
+
